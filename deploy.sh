4d2d4855da9bf3ec459e3a532362e3be69b4918b
Specialist001 <darking-uz@yandex.ru>
2025-04-22 14:22:55 +0500

    Github Action Deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,18 @@
 # Переходим в директорию с проектом
 cd /var/www/warehouse
 
+git config --global --add safe.directory /var/www/warehouse
+
 # Скачиваем последние изменения из репозитория
 git pull origin main
 
 # Если используется Docker
-docker-compose down
-docker-compose up -d --build
+docker compose down
+docker compose up -d --build
 
-# Или если используется другой способ развертывания
-php artisan migrate --force
+# Ждем, пока контейнер поднимется (по желанию)
+sleep 5
+
+docker compose exec laravel_octane_roadrunner php artisan migrate --force
 
 # Прочие команды для деплоя, например очистка кеша или запуск серверов
